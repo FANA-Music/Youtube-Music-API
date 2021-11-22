5c20b557107a7715ea5f8c2abff480599e6e8f40
fan87 <fan87.TW@gmail.com>
2021-11-22 19:10:52 +0800

    Added Getter

--- src/main/java/tk/fanamusic/youtubemusicapi/objects/YMThumbnailInfo.java
+++ src/main/java/tk/fanamusic/youtubemusicapi/objects/YMThumbnailInfo.java
@@ -1,9 +1,11 @@
 package tk.fanamusic.youtubemusicapi.objects;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 public class YMThumbnailInfo {
 
     @SerializedName("thumbnails") private List<YMThumbnail> thumbnails;
